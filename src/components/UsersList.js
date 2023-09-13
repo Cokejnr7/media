c4ec3f377fb8685f42814f28e438681fafa3d7fe
Kolawole Daniel <kolawolecoke@gmail.com>
2023-09-13 19:32:14 +0100

    Skeleton component with shimmer animation using tailwind css

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,4 +1,24 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
+import Skeleton from "./Skeleton";
+
 const UsersList = () => {
+  const dispatch = useDispatch();
+  const {
+    isLoading,
+    data: users,
+    error,
+  } = useSelector((state) => {
+    return state.users;
+  });
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  if (isLoading) return <Skeleton times={6} className="h-10 w-full" />;
+  if (error) return "error";
+
   return "Users List";
 };
 
--- src/store/thunk/fetchUsers.js
+++ src/store/thunk/fetchUsers.js
@@ -1,7 +1,16 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+
 import axios from "axios";
 
 export const fetchUsers = createAsyncThunk("users/fetch", async () => {
-  const response = await axios.get("https://localhost:3005/users");
+  const response = await axios.get("http://localhost:3005/users");
+  await pause(9000);
   return response.data;
 });
+
+// Development only
+const pause = (delay) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+};
