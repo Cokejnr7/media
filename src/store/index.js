ce9aee6147e57a1032b93947082e81e34de2c16b
Kolawole Daniel <kolawolecoke@gmail.com>
2023-09-13 08:02:53 +0100

    Async Thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./slice/usersSlice";
+import { fetchUsers } from "./thunk/fetchUsers";
 
 const store = configureStore({ reducer: { users: userReducer } });
 
 export default store;
+export { fetchUsers };
