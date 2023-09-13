2d0678dc677856a9d9c6f84c208f6016149351f8
Kolawole Daniel <kolawolecoke@gmail.com>
2023-09-13 20:06:21 +0100

    User add functionality

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../store";
+import { fetchUsers, addUser } from "../store";
 import Skeleton from "./Skeleton";
+import Button from "./Button";
 
 const UsersList = () => {
   const dispatch = useDispatch();
@@ -16,10 +17,32 @@ const UsersList = () => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
+  const handleUserAdd = () => {
+    dispatch(addUser());
+  };
+
   if (isLoading) return <Skeleton times={6} className="h-10 w-full" />;
   if (error) return "error";
 
-  return "Users List";
+  const renderedUsers = users.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="flex flex-ro justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button onClick={handleUserAdd}>+ Add User</Button>
+      </div>
+      {renderedUsers}
+    </div>
+  );
 };
 
 export default UsersList;
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./slice/usersSlice";
 import { fetchUsers } from "./thunk/fetchUsers";
+import { addUser } from "./thunk/addUser";
 
 const store = configureStore({ reducer: { users: userReducer } });
 
 export default store;
-export { fetchUsers };
+export { fetchUsers, addUser };
