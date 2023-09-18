b9d9381c3315570532f5d2695774fdc495d7e91a
Kolawole Daniel <kolawolecoke@gmail.com>
2023-09-18 11:38:07 +0100

    seperated the useThunk hook to a seperate file

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,36 +1,17 @@
-import { useCallback, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
+import useThunk from "../hook/use-thunk";
 import Skeleton from "./Skeleton";
 import Button from "./Button";
 
 const UsersList = () => {
-  const useThunk = (thunk) => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const dispatch = useDispatch();
-
-    const doFetchUsers = useCallback(() => {
-      setIsLoading(true);
-      dispatch(thunk())
-        .unwrap()
-        .then(() => {
-          setIsLoading(false);
-        })
-        .catch((error) => setError("Couldn't fetch users"))
-        .finally(() => setIsLoading(false));
-    }, [thunk, dispatch]);
-
-    return [doFetchUsers, isLoading, error];
-  };
-
   const { data: users } = useSelector((state) => {
     return state.users;
   });
 
   const [doFetchUsers, isLoadingUsers, userFetchError] = useThunk(fetchUsers);
   const [createUser, userCreationLoading, userCreateError] = useThunk(addUser);
-  useThunk(fetchUsers);
 
   useEffect(() => {
     doFetchUsers();
---
+++ src/hook/use-thunk.js
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+import { useCallback, useState } from "react";
+
+const useThunk = (thunk) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+
+  const doFetchUsers = useCallback(() => {
+    setIsLoading(true);
+    dispatch(thunk())
+      .unwrap()
+      .then(() => {
+        setIsLoading(false);
+      })
+      .catch((error) => setError("Couldn't fetch users"))
+      .finally(() => setIsLoading(false));
+  }, [thunk, dispatch]);
+
+  return [doFetchUsers, isLoading, error];
+};
+
+export default useThunk;
