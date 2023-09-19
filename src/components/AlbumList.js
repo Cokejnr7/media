c9b88aef5f2fa13a4d3f2ff602cb901f037327b1
Kolawole Daniel <kolawolecoke@gmail.com>
2023-09-19 21:41:01 +0100

    Album List: display album lists

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,34 @@
-const AlbumList = (user) => {
-  return <div>Albums for {user.name}</div>;
+import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
+
+const AlbumList = ({ user }) => {
+  // the hook returns an object with the following properties
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = "Error loading Albums";
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in album
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>Albums by {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 };
 
 export default AlbumList;
