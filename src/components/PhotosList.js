884ce6ff8f646bcdbd443d86f947349e91818ca9
Kolawole Daniel <kolawolecoke@gmail.com>
2023-09-21 19:41:15 +0100

    created photosList and photosListItem componenet

---
+++ src/components/PhotosList.js
@@ -0,0 +1,5 @@
+const PhotosList = ({ album }) => {
+  return <div></div>;
+};
+
+export default PhotosList;
---
+++ src/components/PhotosListItem.js
@@ -0,0 +1,5 @@
+const PhotosListItem = () => {
+  return <div></div>;
+};
+
+export default PhotosListItem;
